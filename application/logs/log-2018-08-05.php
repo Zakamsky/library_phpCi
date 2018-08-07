b5deb4aed6f8fc0b68034f4f37c615877d09752b
zakamsky <alex.zakamsky@gmail.com>
2018-08-08 00:32:31 +0500

    upload project

---
+++ application/logs/log-2018-08-05.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-08-05 13:06:18 --> 404 Page Not Found: Books/index
+ERROR - 2018-08-05 15:41:31 --> 404 Page Not Found: Libs/books
+ERROR - 2018-08-05 15:59:10 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at E:\open_server\OSPanel\domains\library\application\controllers\libs.php:24) E:\open_server\OSPanel\domains\library\system\core\Common.php 570
+ERROR - 2018-08-05 15:59:10 --> Severity: Error --> Call to undefined method Books_model::get_articles() E:\open_server\OSPanel\domains\library\application\controllers\libs.php 24
+ERROR - 2018-08-05 15:59:37 --> Severity: Notice --> Undefined variable: books E:\open_server\OSPanel\domains\library\application\views\books_view.php 13
+ERROR - 2018-08-05 15:59:37 --> Severity: Warning --> Invalid argument supplied for foreach() E:\open_server\OSPanel\domains\library\application\views\books_view.php 13
+ERROR - 2018-08-05 16:01:09 --> Severity: Notice --> Undefined variable: books E:\open_server\OSPanel\domains\library\application\controllers\libs.php 26
+ERROR - 2018-08-05 16:01:09 --> Severity: Notice --> Undefined variable: genres E:\open_server\OSPanel\domains\library\application\controllers\libs.php 26
+ERROR - 2018-08-05 16:01:09 --> Severity: Notice --> Undefined variable: books E:\open_server\OSPanel\domains\library\application\views\books_view.php 13
+ERROR - 2018-08-05 16:01:09 --> Severity: Warning --> Invalid argument supplied for foreach() E:\open_server\OSPanel\domains\library\application\views\books_view.php 13
+ERROR - 2018-08-05 16:02:04 --> Severity: Notice --> Undefined offset: 1964 E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:02:04 --> Severity: Notice --> Undefined offset: 2018 E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:02:04 --> Severity: Notice --> Undefined offset: 1998 E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:02:04 --> Severity: Notice --> Undefined offset: 1917 E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:19:28 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:19:28 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:19:28 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:19:28 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:20:15 --> Severity: Parsing Error --> syntax error, unexpected 'echo' (T_ECHO) E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:20:56 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:20:56 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:20:56 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:20:56 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:21:33 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:21:33 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:21:33 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:21:33 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 19
+ERROR - 2018-08-05 16:24:38 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 16:24:38 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 16:24:38 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 16:24:38 --> Severity: Notice --> Array to string conversion E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 16:39:46 --> Severity: Parsing Error --> syntax error, unexpected '=>' (T_DOUBLE_ARROW), expecting '(' E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 16:40:02 --> Severity: Parsing Error --> syntax error, unexpected '[', expecting '(' E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 16:41:56 --> Severity: Notice --> Undefined variable: books E:\open_server\OSPanel\domains\library\application\controllers\libs.php 26
+ERROR - 2018-08-05 17:13:45 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at E:\open_server\OSPanel\domains\library\application\controllers\libs.php:30) E:\open_server\OSPanel\domains\library\system\core\Common.php 570
+ERROR - 2018-08-05 17:13:45 --> Severity: Parsing Error --> syntax error, unexpected '}' E:\open_server\OSPanel\domains\library\application\controllers\libs.php 30
+ERROR - 2018-08-05 17:14:13 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at E:\open_server\OSPanel\domains\library\application\controllers\libs.php:30) E:\open_server\OSPanel\domains\library\system\core\Common.php 570
+ERROR - 2018-08-05 17:14:13 --> Severity: Parsing Error --> syntax error, unexpected '}' E:\open_server\OSPanel\domains\library\application\controllers\libs.php 30
+ERROR - 2018-08-05 17:14:16 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at E:\open_server\OSPanel\domains\library\application\controllers\libs.php:30) E:\open_server\OSPanel\domains\library\system\core\Common.php 570
+ERROR - 2018-08-05 17:14:16 --> Severity: Parsing Error --> syntax error, unexpected '}' E:\open_server\OSPanel\domains\library\application\controllers\libs.php 30
+ERROR - 2018-08-05 17:17:34 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at E:\open_server\OSPanel\domains\library\application\controllers\libs.php:31) E:\open_server\OSPanel\domains\library\system\core\Common.php 570
+ERROR - 2018-08-05 17:17:34 --> Severity: Parsing Error --> syntax error, unexpected '<' E:\open_server\OSPanel\domains\library\application\controllers\libs.php 31
+ERROR - 2018-08-05 18:56:22 --> Severity: Parsing Error --> syntax error, unexpected '}', expecting ',' or ';' E:\open_server\OSPanel\domains\library\application\views\books_view.php 25
+ERROR - 2018-08-05 18:56:36 --> Severity: Notice --> Undefined variable: genre E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 18:56:36 --> Severity: Notice --> Undefined variable: genre E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 18:56:36 --> Severity: Notice --> Undefined variable: genre E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 18:56:36 --> Severity: Notice --> Undefined variable: genre E:\open_server\OSPanel\domains\library\application\views\books_view.php 20
+ERROR - 2018-08-05 18:57:43 --> Severity: Parsing Error --> syntax error, unexpected 'as' (T_AS) E:\open_server\OSPanel\domains\library\application\views\books_view.php 22
+ERROR - 2018-08-05 20:53:32 --> 404 Page Not Found: Add_book/index
+ERROR - 2018-08-05 20:53:49 --> Severity: Error --> Call to undefined function base_url() E:\open_server\OSPanel\domains\library\application\views\add_article_view.php 11
+ERROR - 2018-08-05 20:55:10 --> Severity: Error --> Call to undefined function base_url() E:\open_server\OSPanel\domains\library\application\views\add_article_view.php 11
+ERROR - 2018-08-05 20:55:11 --> Severity: Error --> Call to undefined function base_url() E:\open_server\OSPanel\domains\library\application\views\add_article_view.php 11
+ERROR - 2018-08-05 20:55:18 --> Severity: Error --> Call to undefined function base_url() E:\open_server\OSPanel\domains\library\application\views\add_article_view.php 11
+ERROR - 2018-08-05 20:59:08 --> Severity: Parsing Error --> syntax error, unexpected end of file E:\open_server\OSPanel\domains\library\application\views\add_book_view.php 30
+ERROR - 2018-08-05 20:59:44 --> Severity: Error --> Call to undefined function base_url() E:\open_server\OSPanel\domains\library\application\views\add_book_view.php 11
+ERROR - 2018-08-05 21:00:30 --> Severity: Error --> Call to undefined function base_url() E:\open_server\OSPanel\domains\library\application\views\add_book_view.php 11
+ERROR - 2018-08-05 21:02:17 --> Severity: Notice --> Undefined variable: books E:\open_server\OSPanel\domains\library\application\views\add_book_view.php 20
+ERROR - 2018-08-05 21:02:17 --> Severity: Warning --> Invalid argument supplied for foreach() E:\open_server\OSPanel\domains\library\application\views\add_book_view.php 20
+ERROR - 2018-08-05 21:12:24 --> Query error: Table 'library.articles' doesn't exist - Invalid query: INSERT INTO `articles` (`title`, `text`, `date`) VALUES ('Жил был пёс', NULL, '2018-08-05')
+ERROR - 2018-08-05 21:21:53 --> Query error: Unknown column 'genre' in 'field list' - Invalid query: INSERT INTO `books` (`title`, `author`, `genre`, `year`) VALUES ('Жил был пёс', 'Пёсий Сын', '10', '2013')
+ERROR - 2018-08-05 22:41:57 --> 404 Page Not Found: Libs/index
+ERROR - 2018-08-05 22:42:07 --> 404 Page Not Found: Libs/index
+ERROR - 2018-08-05 22:42:21 --> 404 Page Not Found: Libs/index
+ERROR - 2018-08-05 22:51:26 --> Severity: Notice --> Undefined property: Libs::$articles_model E:\open_server\OSPanel\domains\library\application\controllers\libs.php 61
+ERROR - 2018-08-05 22:51:26 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at E:\open_server\OSPanel\domains\library\system\core\Exceptions.php:271) E:\open_server\OSPanel\domains\library\system\core\Common.php 570
+ERROR - 2018-08-05 22:51:26 --> Severity: Error --> Call to a member function del_book() on a non-object E:\open_server\OSPanel\domains\library\application\controllers\libs.php 61
---
+++ application/logs/log-2018-08-07.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-08-07 11:29:06 --> 404 Page Not Found: Faviconico/index
+ERROR - 2018-08-07 16:35:16 --> 404 Page Not Found: Libs/css
+ERROR - 2018-08-07 16:35:50 --> 404 Page Not Found: Libs/css
+ERROR - 2018-08-07 16:36:14 --> 404 Page Not Found: Libs/css
+ERROR - 2018-08-07 16:36:21 --> 404 Page Not Found: Libs/css
+ERROR - 2018-08-07 16:49:02 --> 404 Page Not Found: Libs/css
+ERROR - 2018-08-07 16:49:09 --> 404 Page Not Found: Css/bootstrap.min.css
+ERROR - 2018-08-07 17:16:19 --> 404 Page Not Found: Css/bootstrap.min.css
+ERROR - 2018-08-07 17:17:10 --> 404 Page Not Found: Css/bootstrap.min.css
+ERROR - 2018-08-07 17:17:23 --> 404 Page Not Found: Css/bootstrap.min.css
+ERROR - 2018-08-07 18:24:14 --> Severity: Warning --> Missing argument 1 for Libs::edit_book() E:\open_server\OSPanel\domains\library\application\controllers\libs.php 51
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined variable: id E:\open_server\OSPanel\domains\library\application\controllers\libs.php 53
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined variable: id E:\open_server\OSPanel\domains\library\application\controllers\libs.php 55
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 12
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 15
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 18
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 23
+ERROR - 2018-08-07 18:24:14 --> Severity: Notice --> Undefined offset: 0 E:\open_server\OSPanel\domains\library\application\views\edit_book_view.php 28
+ERROR - 2018-08-07 18:42:17 --> 404 Page Not Found: Faviconico/index
+ERROR - 2018-08-07 18:42:17 --> 404 Page Not Found: Faviconico/index
+ERROR - 2018-08-07 19:18:22 --> 404 Page Not Found: Faviconico/index
+ERROR - 2018-08-07 21:52:47 --> 404 Page Not Found: 6565/index
