b4aeb2eda26ac4f1d4425fab464d3f98bc64be6a
zakamsky <alex.zakamsky@gmail.com>
2018-08-08 16:45:50 +0500

    upload project

--- application/views/edit_book_view.php
+++ application/views/edit_book_view.php
@@ -10,41 +10,7 @@
 	<title>Изменить книгу</title>
 </head>
 <body>
-<<!--div class="container"><h1 class="m-5 text-info">Добавить книгу</h1>
 
-	<div class="form border border-info rounded p-4">
-		<form method="post" action="<?/*= base_url() */?>libs/edit_book/<?/*= $book['0']['id']; */?>">
-
-			<div class="form-group pb-3">
-				<label for="title">Название</label>
-				<input type="text" name="title" id="title" value="<?/*= $book['0']['title']; */?>" class="form-control"><?/*= form_error( 'title' ) */?>
-			</div>
-
-			<div class="form-group pb-3">
-				<label for="author">Автор</label>
-				<input type="text" name="author" id="author"
-						 value="<?/*= $book['0']['author']; */?>" class="form-control"><?/*= form_error( 'author' ) */?>
-			</div>
-
-			<div class="form-group pb-3">
-				<label for="genre">Жанр</label>
-				<select name="genre_id" id="genre" class="form-control">
-				<?php /*foreach ( $genres as $genre ): */?>
-						 <option <?php /*if ( $genre['id'] == $book['0']['genre_id'] ) {
-					   echo 'selected';
-				   } */?> value="<?/*= $genre['id'] */?>"><?/*= $genre['genre'] */?></option>
-				<?php /*endforeach; */?>
-				</select>
-			</div>
-
-			<div class="form-group pb-3">
-				<label for="year">Год выпуска</label>
-				<input type="text" name="year" id="year" value="<?/*= $book['0']['year']; */?>" class="form-control"><?/*= form_error( 'year' ) */?>
-			</div>
-
-			<input type="submit" name="add" value="Добавить книгу">
-		</form>
-	</div>-->
 
 <div class="container">
 	<h1 class="m-5 text-info">Изменить книгу</h1>
